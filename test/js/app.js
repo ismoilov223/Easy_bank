dc6f7d5999ce06611cb5178d53bbaddc5633cfce
ismoilov223 <shukurulloismolilov832@gmail.com>
2023-01-30 22:03:39 +0500

    git adding

---
+++ test/js/app.js
@@ -0,0 +1,23 @@
+// var t = prompt("Tezlik kiriting")
+
+function speed(t) {
+  switch (t) {
+    case 70:
+        console.log("yaxshi ketyabsiz");
+      break;
+    case 80:
+        console.log("2000 som jarima");
+      break;
+    case 90:
+        console.log("50000 som jarima");
+      break;
+    case 120:
+        console.log("500000 som jarima");
+      break;
+  
+    default:
+      break;
+    }
+  };
+  speed(80);
+  // console.log(a);
